ea5502b47331b5d473a8a750dc4a2ee75ddf2063
WebStar324 <webdeveloper713@gmail.com>
2020-01-14 03:41:14 +0100

    fix WeatherCartList component

--- src/components/WeatherCardList.tsx
+++ src/components/WeatherCardList.tsx
@@ -1,33 +1,27 @@
-import React from 'react';
-import {
-  Card,
-  Col,
-  Row,
-  CardHeader
-} from "shards-react";
+import React from "react";
+import { Card, Col, Row, CardHeader } from "shards-react";
 
 // Import Components
-import WeatherCard from './WeatherCard';
+import WeatherCard from "./WeatherCard";
 
 // Import Constants
-import { Days } from '../constants/days';
+import { Days } from "../constants/days";
 
 // Import Helpers
-import { checkEndOfDay } from '../helpers/time';
+import { checkEndOfDay } from "../helpers/time";
 
 // Import Types
-import { IWeatherForecastListItem } from '../types/weather';
+import { IWeatherForecastListItem } from "../types/weather";
 
 // Items per Raw
 const itemInRawLength = 4;
 
 interface IProps {
-  weatherDataList: IWeatherForecastListItem[]
+  weatherDataList: IWeatherForecastListItem[];
 }
 
 // this component render all the weather informations provided by the API for a given time
 const WeatherCardList = (props: IProps) => {
-
   const { weatherDataList } = props;
 
   // Props required to render the reactAnimatedWeather Component
@@ -36,29 +30,40 @@ const WeatherCardList = (props: IProps) => {
     let row: JSX.Element[] = [];
     let daily: JSX.Element[] = [];
 
-
-    for (let index = 0; index < list.length; index++) {
-      const isLastWeatherForecastOfDay = checkEndOfDay(new Date(list[index].dt_txt));
-
-      daily.push(<Col key={`Col-${index}`}>
-        <WeatherCard weatherData={list[index]} />
-      </Col>)
-      if (daily.length === itemInRawLength || isLastWeatherForecastOfDay) {
-        row.push(<Row key={`Row-${index}`}>{daily}</Row>)
-        daily = []
+    if (list && list.length > 0) {
+      for (let index = 0; index < list.length; index++) {
+        const isLastWeatherForecastOfDay = checkEndOfDay(
+          new Date(list[index].dt_txt)
+        );
+        daily.push(
+          <Col key={`Col-${index}`}>
+            <WeatherCard weatherData={list[index]} />
+          </Col>
+        );
+        if (daily.length === itemInRawLength || isLastWeatherForecastOfDay) {
+          row.push(<Row key={`Row-${index}`}>{daily}</Row>);
+          daily = [];
+        }
+        if (isLastWeatherForecastOfDay) {
+          fiveDays.push(
+            <Card
+              className="mt-2 mb-2 w-100"
+              style={{ opacity: 0.89 }}
+              key={`Card-${index}`}
+            >
+              <CardHeader>
+                <h2>{Days[fiveDays.length]}</h2>
+              </CardHeader>
+              {row}
+            </Card>
+          );
+          row = [];
+        }
       }
-      if (isLastWeatherForecastOfDay) {
-        fiveDays.push(<Card className="mt-2 mb-2 w-100" style={{ opacity: 0.89 }} key={`Card-${index}`}> <CardHeader><h2>{Days[fiveDays.length]}</h2></CardHeader>{row}</Card>)
-        row = []
-      }
+      return fiveDays;
     }
-    return fiveDays
-  }
+  };
 
-  return (
-    <>
-      {renderWeatherCards(weatherDataList)}
-    </>
-  )
-}
+  return <>{renderWeatherCards(weatherDataList)}</>;
+};
 export default WeatherCardList;
